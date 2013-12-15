bb187286181dd29bfa2f35e0d13af899a8f7d71a
Anthony Bouch <tony@58bits.com>
2013-12-15 11:35:28 +0700

    Added created_at date to seeds.

--- app/database/seeds/CarrierTypesTableSeeder.php
+++ app/database/seeds/CarrierTypesTableSeeder.php
@@ -11,14 +11,17 @@ class CarrierTypesTableSeeder extends Seeder {
             array(
                 'name'          => 'Ampex 631 1/4" x 1200\', 7" Reel, 7.5ins',
                 'description'   => 'Ampex Professional Audio Tape',
+                'created_at'    => date('Y-m-d H:i:s'),
             ),
             array(
                 'name'          => 'Audiotape 1/4" x 1200\', 7" Reel, 7.5ins',
                 'description'   => 'Audio Devices Inc. Audiotape',
+                'created_at'    => date('Y-m-d H:i:s'),
             ),
             array(
                 'name'          => 'Scotch 3M 1/4 1/4" x 1200\', 7" Reel, 7.5ins',
                 'description'   => 'Scotch 3M 206 Reel to Reel Tape',
+                'created_at'    => date('Y-m-d H:i:s'),
             )
         );
 
