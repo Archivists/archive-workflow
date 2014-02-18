61107030a6e0dfbc06926aca0bbd3d752fd31f77
Anthony Bouch <tony@58bits.com>
2014-02-18 10:58:07 +0500

    Further improvements to artifact preview layout.

--- app/views/artifact/_details.blade.php
+++ app/views/artifact/_details.blade.php
@@ -17,7 +17,14 @@
 @if (preg_match('/^.*\.(jpg|jpeg|png|gif|tiff|tif)$/i', $artifact->name) === 1)
 	<img style="width: 100%;" src="{{{ URL::to('/artifact/' . $carrier->archive_id . '/previews/' . $artifact->name ) }}}"/>
 @else
-	<img src="{{{ URL::to('/artifact/' . $carrier->archive_id . '/thumbnails/' . $artifact->name ) }}}"/>
+	<img style="float: left; margin-right: 10px;" src="{{{ URL::to('/artifact/' . $carrier->archive_id . '/thumbnails/' . $artifact->name ) }}}"/>
+	@if ($action == 'show')
+		<p style="margin:10px;">There is no preview available for this artifact, however, you  may download the artifact by clicking on the 'Download' button to the right.</p>
+	@else
+		<p style="margin:10px;">Note: You can permenantly remove this artificact by clicking on the 'Delete' button to the right. This action cannot be undone.</p>
+	@endif
+	
+	
 @endif
 
 <!-- ./ form actions -->
