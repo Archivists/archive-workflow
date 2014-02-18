41d568e4f01e9600f106b0cb5e064c44f20f7da6
Anthony Bouch <tony@58bits.com>
2014-02-18 10:34:41 +0500

    Improved artifact preview layout.

--- app/views/artifact/_details.blade.php
+++ app/views/artifact/_details.blade.php
@@ -1,8 +1,9 @@
 {{-- Atifact Details --}}
 <!-- Form Actions -->
-<div class="form-group form-inline">
+<div class="form-group form-inline pull-right">
+	
 	<div class="controls">
-		<img src="{{{ URL::to('/artifact/' . $carrier->archive_id . '/thumbnails/' . $artifact->name ) }}}"/>
+		
 		@if ($action == 'show')
 			<a href="{{{ URL::to('carriers/' . $carrier->id) }}}" class="btn btn-primary">Close</a>
 			<a href="{{{ URL::to('/artifact/' . $carrier->archive_id . '/download/' . $artifact->name ) }}}" class="btn btn-success">Download Artifact</a>
@@ -15,6 +16,8 @@
 
 @if (preg_match('/^.*\.(jpg|jpeg|png|gif|tiff|tif)$/i', $artifact->name) === 1)
 	<img style="width: 100%;" src="{{{ URL::to('/artifact/' . $carrier->archive_id . '/previews/' . $artifact->name ) }}}"/>
+@else
+	<img src="{{{ URL::to('/artifact/' . $carrier->archive_id . '/thumbnails/' . $artifact->name ) }}}"/>
 @endif
 
 <!-- ./ form actions -->
