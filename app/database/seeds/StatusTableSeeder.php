ce79dd8b060c1f3ea27ce2dba8d8117b60194a98
Anthony Bouch <tony@58bits.com>
2014-02-21 18:22:56 +0500

    Added QC and Part Editing to DB Status Seed.

--- app/database/seeds/StatusTableSeeder.php
+++ app/database/seeds/StatusTableSeeder.php
@@ -21,14 +21,21 @@ class StatusTableSeeder extends Seeder {
                 'created_at'    => date('Y-m-d H:i:s'),
             ),
             array(
-                'name'          => 'Complete',
+                'name'          => 'QC and Part Editing',
                 'order'         => 3,
+                'description'   => 'The carrier is being checked.',
+                'created_at'    => date('Y-m-d H:i:s'),
+            ),
+            array(
+                'name'          => 'Complete',
+                'order'         => 4,
                 'description'   => 'Preparation and digitisation are complete, and the digital objects including all metadata have been bundled and sent for ingest into the catalogue.',
                 'created_at'    => date('Y-m-d H:i:s'),
             )
         );
 
         DB::table('status')->insert( $types );
+
     }
 
 }
