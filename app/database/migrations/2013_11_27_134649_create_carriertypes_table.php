c0e0df61093f5b150231fe7b416ec46793710172
Anthony Bouch <tony@58bits.com>
2014-03-01 13:20:17 +0500

    Set default on carrier_type_id

--- app/database/migrations/2013_11_27_134649_create_carriertypes_table.php
+++ app/database/migrations/2013_11_27_134649_create_carriertypes_table.php
@@ -14,7 +14,7 @@ class CreateCarriertypesTable extends Migration {
 	{
 		Schema::create('carrier_types', function(Blueprint $table)
 		{
-			$table->increments('id');
+			$table->increments('id')->default(0);
 			$table->string('name');
 			$table->string('description');
 			$table->string('created_by')->default("");
